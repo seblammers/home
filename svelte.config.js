f33a8ca03e77ca82b08256f3077fc53aee59f275
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-06-11 22:10:54 +0200

    fixed heading-hovers and behaviour

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import  sveltePreprocess  from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,7 +18,11 @@ const config = {
 			extensions: ['.md'],
 			layout: {
 				posts: 'src/routes/posts/_post.svelte'
-			}
+			},
+			rehypePlugins: [
+				rehypeSlug,
+				[rehypeAutolinkHeadings, { behavior: "wrap" } ],
+			]
 		})
 	],
 };
