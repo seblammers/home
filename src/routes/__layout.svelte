6c80b9a2dcbef525ceac0f2305241204876831dd
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-07-23 17:21:12 +0200

    update nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,25 +11,25 @@
 </script>
 
 <script>
-    import Header from '$lib/components/Header.svelte';
+    import Nav from '$lib/components/Nav.svelte';
     import Footer from '$lib/components/Footer.svelte';
     import '$lib/styles/global.scss';
     import "@fontsource/inter/400.css";
     import "@fontsource/inter/500.css";
     import "@fontsource/inter/700.css";
     import "@fontsource/inter/900.css";
-    import { fade } from 'svelte/transition'
+    import { fade, fly } from 'svelte/transition'
 
     export let currentRoute;
 
 </script>
   
 <div class="layout">
-  <Header />
+  <Nav />
 
   {#key currentRoute}
     <main 
-      in:fade={{ duration: 250, delay: 500 }} 
+      in:fly={{ y: 100, duration: 800, delay: 250 }} 
       out:fade={{ duration: 250 }}
       class="flow"
       >
