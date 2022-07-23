9f2451ffde5a54ce7e1a90628efc2d0c28e9d560
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-07-23 17:16:38 +0200

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,49 @@
+<script>
+  import { navItems, siteAuthor } from '$lib/config';
+  import { navigating, page } from '$app/stores';
+
+  $: isActive = (url) => $page.url.pathname.includes(url);
+</script>
+
 <footer>
-  <p>Hello, I'm the footer.</p> 
+  <p>This site does not track you. Like at all.</p> 
   
-  <p>&copy; {new Date().getFullYear()} Well, me.</p>
+  <p>&copy; {new Date().getFullYear()} {siteAuthor}</p>
+
+  <!-- <div class="links">
+    {#each navItems as page, i (i)}
+        <a
+            href={page.route}
+            class:active={isActive(`${page.route}`)}>
+            {page.title}
+        </a>
+    {/each}
+</div> -->
 </footer>
 
-  <!-- <style>
-    footer {
-      padding: 1rem;
-      background: inherit;
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      flex-shrink: 0;
-    }
-  
-  </style> -->
+<style lang="scss">
+  footer {
+  background: var(--ink);
+  color: var(--paper);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--space-m) var(--space-m);
+
+  p {
+    margin: 0;
+    font-size: var(--step-0);
+  }
+}
+//   .links {
+//     margin: 0px auto;
+//     display: flex;
+//     flex-direction: column;
+//     gap: var(--space-s);
+//     a {
+//         font-size: var(--step-1);
+//         border-width: 2px;
+//         // border: none;
+//     }
+// }
+</style>
