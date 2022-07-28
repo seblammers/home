7f537f9ea7417027e6502bc22954cd2d837c0376
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-07-28 10:03:09 +0200

    init [category].svelte

--- src/routes/posts/categories/[category].svelte
+++ src/routes/posts/categories/[category].svelte
@@ -9,8 +9,33 @@
   
       return {
         props: {
-          posts: matchingPosts
+          posts: matchingPosts,
+          category: currentCategory
         }
       }
     }
+    
   </script>
+
+<script>
+  import Card from '$lib/components/Card.svelte'
+  export let posts
+  export let category
+</script>
+
+{#if posts.length}
+<article class="flow">
+  
+<h1>Content filed under "{category}"</h1>
+  {#each posts as post}
+    <Card {post} />
+  {/each}
+  
+ </article>
+
+{:else}
+  <h1>Whoops!</h1> 
+  <p>Sorry, couldn't find any content filed under "{category}".</p>
+
+  <p><a href="/posts">Back to Posts</a></p>
+{/if}
