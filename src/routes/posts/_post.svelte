11b363a062c5b789feea40f8ef20667fa144d491
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-07-28 10:02:36 +0200

    update post template

--- src/routes/posts/_post.svelte
+++ src/routes/posts/_post.svelte
@@ -1,29 +1,78 @@
 <script>
-  import { fly } from 'svelte/transition'
+  import { siteURL, siteAuthor } from '$lib/config';
+  import { page } from '$app/stores';
+  import { dateFormat } from '$lib/assets/js/utils';
+  import Readotron from "$lib/components/Readotron.svelte";
+  import Heading from "$lib/components/Heading.svelte";
+  import { Head } from 'svead';
   export let title
   export let date
   export let categories
+  export let updated
+  export let description
+  let url = $page.url.toString
+  let authorName = siteAuthor
+  let website = siteURL
+  let image = `https://ik.imagekit.io/seblammers/tr:otf-Inter-SemiBold_zoEu2Lj-l.otf,ot-${title},ots-72,otc-FFF,ox-10,oy-20,otw-700/twittercard_-1cx8-LQN.png`
+
+$: console.log(image);
 </script>
   
-  <h1 in:fly={{ x: -200, duration: 250, delay: 500 }} >{title}</h1>
+<Head {title} {description} {image} {url} {authorName} {website} />
+
+<Heading>
+  {title}
+</Heading>
+
+<div class="meta">
   
-  <p>Published: {date}</p>
-  
-  <article class="post flow">
-    <slot />  
-  </article>
+  <time>Published: {dateFormat(date)} </time>
+
+  {#if typeof updated !== 'undefined'}
+    <time>Updated: {dateFormat(updated)}</time>
+  {/if}
+
+  <Readotron selector=".post" template={(time) => `Reading Time: ${time} ${(time > 1 ? "minutes" : "minute")}`}/>
+
 
   {#if categories.length}
-    <aside class="flow">
-      <h3>Posted in:</h3>
-      <ul>
-        {#each categories as category}
-          <li>
-            <a href="/posts/categories/{category}">
-              {category}
-            </a>
-          </li>
-        {/each}
-      </ul>
-    </aside>
-  {/if}
+  <div class="flow tags">
+    <ul class="meta">
+      {#each categories as category}
+        <li>
+          <a class="pill" href="/posts/categories/{category}">
+            {category}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+{/if}
+
+</div>
+
+<article class="post flow">
+  <slot />  
+</article>
+
+<style lang="scss">
+
+  .meta {
+    flex-direction: row;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 32rem;
+    margin: 0;
+    gap: var(--space-2xs);
+    font-size: var(--step-0);
+  }
+
+  ul {
+      list-style-type: none;
+      list-style: none;
+      padding-left: 0%;
+    }
+
+</style>
