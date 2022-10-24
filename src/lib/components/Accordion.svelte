6b095231db6a84c06b59be6f4a40579907e135b7
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-10-24 22:01:53 +0200

    polish accordion

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -1,58 +1,58 @@
 <script>
-    export let open = false;
-      import { slide } from 'svelte/transition';
-      const handleClick = () => open = !open
-  </script>
-  
-  <div class="accordion">
-      <div class="header" on:click={handleClick} >
-          <div class="text">
-              <b>Show me the code</b>
-          </div>
-          <div class="tap">
-              {#if open}
-              tap me to hide details
-              {:else}
-              tap me to show details
-              {/if}
-          </div>
-  </div>
-          {#if open}
-      <div class="details" transition:slide>
-          <slot/>
-      </div>
-      {/if}
-  </div>
-  
-  <style>
-      div.accordion {
-          margin: 1rem 0;
-          font-family: var(--accentFont);
-      }
-      
-      div.header {
-          display: flex;
-          flex-direction: column;
-          width:100%;
-          background: #ddd;
-          padding: 1rem;
-      }
-      
-      div.header:hover {
-      cursor: pointer;
-      }
-      div.header .text {
-          flex: 1;
-          margin-right: 5px;
-      }
-  
-      div.details {
-          /* background-color: #cecece; */
-          padding: 1rem;
-      }
-      
-      .tap {
-          font-style: italic;
-      }
-  </style>
-  
+	// if custom summary is not provided, fall back on "Show me the code"
+	// which is probably the most frequent use-case
+	export let summary = 'Show me the code';
+</script>
+
+<details class="accordion">
+	<summary>
+		<b>{summary}</b>
+	</summary>
+
+	<div class="details">
+		<slot />
+	</div>
+</details>
+
+<style>
+	details {
+		border: var(--radius) solid var(--accent);
+		border-radius: var(--radius);
+		padding: var(--space-s);
+		margin: var(--space-xs) 0;
+		max-width: var(--max-width);
+		border-style: none none none solid;
+		background-color: var(--surface4-light);
+		font-family: var(--accentFont);
+	}
+
+	/* # The Rotating Marker # */
+	/* hattip https://codepen.io/redesigned/pen/wvoEvqG */
+	summary {
+		display: block;
+		position: relative;
+		cursor: pointer;
+		padding: 1rem 2rem;
+		padding-left: 2.9rem;
+	}
+
+	/* # The Rotating Marker # */
+	details summary::-webkit-details-marker {
+		display: none;
+	}
+
+	summary::before {
+		content: '▶';
+		position: absolute;
+		top: 1rem;
+		left: 0.8rem;
+		transform: rotate(0);
+		transform-origin: center;
+		transition: 0.25s transform ease;
+	}
+
+	details[open] > summary:before {
+		transform: rotate(90deg);
+		transition: 0.25s transform ease;
+	}
+</style>
