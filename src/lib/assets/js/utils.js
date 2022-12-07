79f38b98302f3ff09072e5b11520feabfe8c86a0
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-12-07 22:15:38 +0100

    fix date to english

--- src/lib/assets/js/utils.js
+++ src/lib/assets/js/utils.js
@@ -2,7 +2,7 @@
 // h/t connor rothschild (again :)
 export function dateFormat(date) {
     const parsed = new Date(date);
-    const month = parsed.toLocaleString("default", { month: "long" });
+    const month = parsed.toLocaleString("en-GB", { month: "long" });
     const year = parsed.getUTCFullYear();
 
     return `${month} ${year}`;
